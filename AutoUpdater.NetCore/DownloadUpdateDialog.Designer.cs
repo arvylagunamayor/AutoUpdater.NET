fc7b0d31b3d97cd8639b9e278266ec9df807357c
Arvy <almayor.itd.pps@gmail.com>
2020-04-02 15:40:16 +0800

    Added DownloadUpdateDialog

---
+++ AutoUpdater.NetCore/DownloadUpdateDialog.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace AutoUpdaterDotNetCore
+{
+    partial class DownloadUpdateDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelSize = new System.Windows.Forms.Label();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelSize
+            // 
+            this.labelSize.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelSize.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelSize.Location = new System.Drawing.Point(264, 14);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(135, 15);
+            this.labelSize.TabIndex = 7;
+            this.labelSize.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.AutoSize = true;
+            this.labelInformation.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.labelInformation.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.labelInformation.Location = new System.Drawing.Point(77, 14);
+            this.labelInformation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(128, 15);
+            this.labelInformation.TabIndex = 6;
+            this.labelInformation.Text = "Downloading Update...";
+            // 
+            // progressBar
+            // 
+            this.progressBar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.progressBar.Location = new System.Drawing.Point(80, 45);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(319, 26);
+            this.progressBar.TabIndex = 5;
+            // 
+            // pictureBoxIcon
+            // 
+            this.pictureBoxIcon.Image = global::AutoUpdaterDotNetCore.Properties.Resources.download_32;
+            this.pictureBoxIcon.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBoxIcon.Location = new System.Drawing.Point(13, 14);
+            this.pictureBoxIcon.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxIcon.Name = "pictureBoxIcon";
+            this.pictureBoxIcon.Size = new System.Drawing.Size(59, 62);
+            this.pictureBoxIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBoxIcon.TabIndex = 4;
+            this.pictureBoxIcon.TabStop = false;
+            // 
+            // DownloadUpdateDialog2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 90);
+            this.Controls.Add(this.labelSize);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.pictureBoxIcon);
+            this.Name = "DownloadUpdateDialog2";
+            this.Text = "DownloadUpdateDialog2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelSize;
+        private System.Windows.Forms.Label labelInformation;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.PictureBox pictureBoxIcon;
+    }
+}
